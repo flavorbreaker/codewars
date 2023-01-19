54ba52bdb932247df1a452e30df72e451d9d5dfe
flavorbreaker <turner.l.anthony@gmail.com>
2023-01-18 20:45:31 -0500

    added commented header with CW info

--- summixedarray.kt
+++ summixedarray.kt
@@ -1,3 +1,9 @@
+//Title: Sum Mixed Array
+//Language: Kotlin
+//Description:
+    //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+    //Return your answer as a number.
+
 public class MixedSum {
 
     /*
